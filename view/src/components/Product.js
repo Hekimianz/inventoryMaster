b38abe1bf0386b893bb14aefcb7f876666ed946a
Hekimianz <aramhekdesign@gmail.com>
2024-03-14 11:09:45 -0600

    implemented add by one to quantities

--- view/src/components/Product.js
+++ view/src/components/Product.js
@@ -1,13 +1,36 @@
 import styles from "./css/Product.module.css";
+import { useState } from "react";
 function Product(props) {
+  const [quantity, setQuantity] = useState(props.quant);
+  const handleIncrement = async () => {
+    try {
+      const response = await fetch(
+        `https://inventorymaster-api.onrender.com/${props.id}`,
+        {
+          method: "POST",
+        }
+      );
+      if (response.ok) {
+        setQuantity(quantity + 1);
+      } else {
+        console.error("Failed to increment quantity.");
+      }
+    } catch (err) {
+      console.error("Error incrementing quantity:", err);
+    }
+  };
   return (
     <div className={styles.productCont}>
       <p className={styles.productId}>{props.id}</p>
       <p className={styles.productName}>{props.name}</p>
       <div className={styles.quantityCont}>
         {props.isLegend ? null : <span className={styles.decrement}>-</span>}
-        <p className={styles.productQuantity}>{props.quant}</p>
-        {props.isLegend ? null : <span className={styles.increment}>+</span>}
+        <p className={styles.productQuantity}>{quantity}</p>
+        {props.isLegend ? null : (
+          <span className={styles.increment} onClick={handleIncrement}>
+            +
+          </span>
+        )}
       </div>
     </div>
   );
